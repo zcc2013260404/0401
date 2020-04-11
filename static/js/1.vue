4c8b00d829459f9e90693edd1b2a94f7beaaec1e
zcc2013260404 <2578759549@qq.com>
2020-04-11 18:18:57 +0800

    ss

---
+++ static/js/1.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div>dfdfdfdfdfdf</div>
+</template>
+<script>
+export default {
+    name:"jf",
+    methods: {
+       gt(){
+           //zccc
+           console.log("hahha")
+       },
+        lt(){
+           //zccc
+           console.log("ass")
+       }
+    }
+    
+}
+</script>
